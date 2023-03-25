ae065064204dad062ce498150e120fed6ea1c85a
Christopher Cosma <cosma.5@osu.edu>
2023-03-25 17:12:44 -0400

    Add Deno Deploy check

--- mongo.ts
+++ mongo.ts
@@ -3,9 +3,17 @@ import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 const client = new MongoClient();
 
-const mongo_config = config();
-const username = mongo_config.USERNAME;
-const password = mongo_config.PASSWORD;
+let username, password;
+
+const isDenoDeploy = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
+if (isDenoDeploy) {
+    username = Deno.env.get("USERNAME");
+    password = Deno.env.get("PASSWORD");
+} else {
+    const mongo_config = config();
+    username = mongo_config.USERNAME;
+    password = mongo_config.PASSWORD;
+}
 
 await client.connect(
     `mongodb+srv://${username}:${password}@cluster0.v5uacbb.mongodb.net/readings?retryWrites=true&w=majority&authMechanism=SCRAM-SHA-1`
